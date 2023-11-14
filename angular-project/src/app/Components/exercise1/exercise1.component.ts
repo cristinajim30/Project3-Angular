fc67c9ca125ee1956bbf562c30d524b2b775b393
Cristina <c>
2023-11-14 13:37:29 +0100

    name and font family finished

--- angular-project/src/app/Components/exercise1/exercise1.component.ts
+++ angular-project/src/app/Components/exercise1/exercise1.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exercise1',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './exercise1.component.html',
   styleUrl: './exercise1.component.css'
 })
-export class Exercise1Component {
-  userName: string = 'Name';
+export class Exercise1Component  {
+  userName: string = ''.toUpperCase();
+  fontList: string[] = ['Poppins','Roboto Mono', 'Dancing Script', 'Josefin Sans'];
+ 
+
+  optionSelected: string = this.fontList[0]; 
+  fontSelected: string = this.fontList[0];
+  
+  /*getName():void{
+    console.log('Valor del input:', this.userName);
+    //this.userName = this.userName;
+  }*/
+
+  selectOption(): void {
+    console.log('Opción seleccionada:', this.optionSelected);
+    this.fontSelected = this.optionSelected;
+
+  }
+  
 }
+
+
