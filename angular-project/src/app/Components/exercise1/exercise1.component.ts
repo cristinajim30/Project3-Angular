c0c7a70cedad7d96e3a765cf2ac622f065975b0e
Cristina <c>
2023-11-14 09:44:06 +0100

    create project and first component

---
+++ angular-project/src/app/Components/exercise1/exercise1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-exercise1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './exercise1.component.html',
+  styleUrl: './exercise1.component.css'
+})
+export class Exercise1Component {
+
+}
---
+++ angular-project/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { Exercise1Component } from './Components/exercise1/exercise1.component';
+
+export const routes: Routes = [
+    {path: 'Home', component: Exercise1Component},
+];
