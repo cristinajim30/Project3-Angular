beb8efc5289a5c7d6e09e825dd2fc1c920270ce0
Cristina <c>
2023-11-15 12:17:57 +0100

    trying install bootstrap failed

--- angular-project/src/app/Components/exercise2/exercise2.component.ts
+++ angular-project/src/app/Components/exercise2/exercise2.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exercise2',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './exercise2.component.html',
   styleUrl: './exercise2.component.css'
 })
 export class Exercise2Component {
+  number1: number = 0;
+  number2: number = 0;
+  result: number = 0;
+  operationList: string[] =['add', 'subtract', 'multiply', 'divide']
+  selectedOperation: string = this.operationList[0]; // add operation by default
 
+  calculate(): void {
+    switch (this.selectedOperation) {
+      case this.operationList[0]:
+        //add
+        this.result = this.number1 + this.number2;
+        break;
+      case this.operationList[1]:
+        //subtract
+        this.result = this.number1 - this.number2;
+        break;
+      case this.operationList[2]:
+        //multiply
+        this.result = this.number1 * this.number2;
+        break;
+      case this.operationList[3]:
+        //divide
+        this.result = this.number1 / this.number2;
+        break;
+      default:
+        this.result = 0;
+    }
+  }
 }
